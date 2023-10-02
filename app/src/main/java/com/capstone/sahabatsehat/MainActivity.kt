b0cea58cde5ae4e4a1db4de8f1478aabafc67816
ricoasmara <muhammadricoasmara31@gmail.com>
2023-10-02 23:56:10 +0700

    Splash Screen v1.0

--- app/src/main/java/com/capstone/sahabatsehat/MainActivity.kt
+++ app/src/main/java/com/capstone/sahabatsehat/MainActivity.kt
@@ -2,10 +2,24 @@ package com.capstone.sahabatsehat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.sahabatsehat.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewpager.adapter=ViewPagerAdapter(this)
+        dotIndicator()
+    }
+
+    private fun dotIndicator(){
+        val adapter=ViewPagerAdapter(this)
+        binding.viewpager.adapter=adapter
+        binding.dotsIndicator.attachTo(binding.viewpager)
     }
 }
---
+++ app/src/main/java/com/capstone/sahabatsehat/OnboardingFragment3.kt
@@ -0,0 +1,29 @@
+package com.capstone.sahabatsehat
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+
+class OnboardingFragment3 : Fragment() {
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_onboarding3, container, false)
+        val startLog= view.findViewById<Button>(R.id.btnStart)
+
+        startLog.setOnClickListener{
+
+        }
+        return view
+    }
+}
