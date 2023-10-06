00333a898b87f2619f7c913319daef2ee747579c
zamzamnurahman <kazamnn1912@gmail.com>
2023-10-06 22:37:11 +0700

    ui(register): add ui register form

--- app/src/main/java/com/capstone/sahabatsehat/MainActivity.kt
+++ app/src/main/java/com/capstone/sahabatsehat/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.capstone.sahabatsehat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.sahabatsehat.ui.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
