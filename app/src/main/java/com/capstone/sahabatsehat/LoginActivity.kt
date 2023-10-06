f3d8394dcf504fc823268d059f3fbe1a612be243
ricoasmara <muhammadricoasmara31@gmail.com>
2023-10-06 23:41:00 +0700

    UI Login

--- app/src/main/java/com/capstone/sahabatsehat/LoginActivity.kt
+++ app/src/main/java/com/capstone/sahabatsehat/LoginActivity.kt
@@ -2,10 +2,32 @@ package com.capstone.sahabatsehat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.sahabatsehat.databinding.ActivityLoginBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setError()
+    }
+
+    private fun setError() {
+        val emailEditText = binding.edEmail
+        val passwordEditText = binding.edPassword
+        val email=emailEditText.text.toString()
+        val password=passwordEditText.text.toString()
+
+        if (email.isEmpty()) {
+            emailEditText.error = "Email harus diisi"
+            return
+        }
+
+        if (password.isEmpty()) {
+            passwordEditText.error = "Password harus diisi"
+            return
+        }
     }
 }
