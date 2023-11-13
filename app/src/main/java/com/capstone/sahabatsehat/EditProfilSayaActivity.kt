63fac8b7a2f47ce605cd770bca67357d17a82f7e
salsharistania <salsharistania1@gmail.com>
2023-11-13 11:52:56 +0700

    Add Layout/Activity

---
+++ app/src/main/java/com/capstone/sahabatsehat/EditProfilSayaActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.sahabatsehat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfilSayaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profil_saya)
+    }
+}
---
+++ app/src/main/java/com/capstone/sahabatsehat/ProfilSayaActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.sahabatsehat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilSayaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_saya)
+    }
+}
